c091d5d5eb581028602b7c3747505d5b2f8e8b2e
Alvaro Castro Cid <58068322+fidotheprince@users.noreply.github.com>
2020-05-04 14:48:55 -0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Massively by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 (function($) {
 
@@ -186,9 +182,10 @@
 
 					// Flip icon classes.
 						$navPanelInner.find('.icons, .icon')
-							.addClass('alt');
+							.addClass('alt');				
 
 				});
+			
 
 			// Hack: Disable transitions on WP.
 				if (browser.os == 'wp'
